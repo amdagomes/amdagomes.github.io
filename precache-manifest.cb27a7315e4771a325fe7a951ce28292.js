83ae0952ad75dfbab655180fd7f9ba23b3d5465f
Amanda Gomes <amandagomes2010@gmail.com>
2019-05-14 09:11:57 -0300

    Updates

---
+++ precache-manifest.cb27a7315e4771a325fe7a951ce28292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d692c2e168e011e9613da0aa9bc1b74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b937ae7663f9fd63993",
+    "url": "/static/css/main.d362e5f5.chunk.css"
+  },
+  {
+    "revision": "b8f3e9f7531aaeb66b26",
+    "url": "/static/js/2.b24f44e6.chunk.js"
+  },
+  {
+    "revision": "8b937ae7663f9fd63993",
+    "url": "/static/js/main.7bb7b364.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b19ba1c698f06b3a213fc7264b00e54e",
+    "url": "/static/media/profile.b19ba1c6.png"
+  },
+  {
+    "revision": "cfa581f50d0fb5880aebdfe3de9ea8f5",
+    "url": "/static/media/wall1.cfa581f5.jpg"
+  }
+]);
